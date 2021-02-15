944d481adcea5db478beeffd9e8cee10aaa76080
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-02-15 19:13:25 +0530

    setup done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+import { v4 as uuidv4 } from 'uuid';
+import {useState} from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([
+    {
+    id:uuidv4(),
+    title:'First item',
+    body:'First body'
+  }, 
+  {
+    id:uuidv4(),
+    title:'Second item',
+    body:'Second body'
+  }
+  ])
+
+  const [item, setItem] = useState({
+    title:'',
+    body:''
+  })
+  const [body, setBody] = useState('')
+  const [editItem, setEditItem] = useState(false)
+
+  const handleChange = (e) =>{
+    console.log('handle chnage')
+  }
+
+  const handleSubmit = (e) => {
+    console.log('handle Submit')
+  }
+
+  const clearList = (e) => {
+    console.log('Clear list')
+  }
+
+  const handleEdit = (id) => {
+    console.log(`handle edit ${id}`)
+  }
+
+  const handleDelete = (id) => {
+    console.log(`handler delete ${id}`)
+  }
+
   return (
     <div className="App">
-        <TodoInput/>
-        <TodoList/>
-        
+        <h1> Todo Input </h1>
+        <TodoInput item={item} handleChange={handleChange} handleSubmit={handleSubmit} editItem={editItem}/>
+        <TodoList items={items} clearList={clearList} handleDelete={handleDelete} handleEdit={handleEdit}/>
+
     </div>
   );
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -9,14 +9,13 @@ const TodoInput = () => {
     return (
         <div>
             <div className="text-center">
-            <h1> Todo Input </h1>
             <form style={{width:'500px', margin:'auto'}} onSubmit={ (e) => e.preventDefault()}>
 
-                <div class="mb-3">
+                <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
                     <input type="text" className="form-control title" id="title" name="title" defaultValue="" onChange={changeNote} placeholder="Enter note title"/>
                 </div>
-                <div class="mb-3">
+                <div className="mb-3">
                     <label htmlFor="body" className="form-label">Body</label>
                     <textarea className="form-control" id="body" rows="3" name="body" defaultValue="" onChange={changeNote} placeholder="Write a note"></textarea>
                 </div>
